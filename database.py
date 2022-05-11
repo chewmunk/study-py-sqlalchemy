4239e57cb7f395af16527d095196bce83d378569
chewmunk <chw1787@gmail.com>
2022-05-11 10:39:58 +0900

    add connect()

--- database.py
+++ database.py
@@ -1,14 +1,37 @@
 import sqlalchemy
 import logging
 
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
 
 logging.getLogger().setLevel(logging.INFO)
+
 
 def check_version():
     logging.info(sqlalchemy.__version__)
 
+def connect(url):
+    engine = create_engine(url, echo=True)
+    return engine
+
+def test_create_table():
+    url = 'sqlite:///data/sqlalchemy.db'
+    engine = connect(url)
+    meta = MetaData()
+
+    students = Table(
+        'students', meta,
+        Column('id', Integer, primary_key = True),
+        Column('name', String),
+        Column('lastname', String),
+    )
+
+    meta.create_all(engine)
+
+    logging.info(engine)
+
+
 def main():
-    check_version()
+    test_create_table()
 
 if __name__ == '__main__':
     main()
